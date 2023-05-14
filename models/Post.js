50d9d67b4d28372da7467a4247a145b1fa8d911c
pramod <pramodahetti05@gmail.com>
2023-05-14 19:51:25 +0530

    added new and delete comment route

--- models/Post.js
+++ models/Post.js
@@ -4,9 +4,16 @@ let post=new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         required:true
        },
+       date:{
+          type:String,
+          required:true
+       },
        message:{
           type:String,
           required:true
+       },
+       comment:{
+         type:Array,
        }
 })
 module.exports=mongoose.model('post',post);
--- routes/api/post.js
+++ routes/api/post.js
@@ -4,11 +4,25 @@ const userauth=require('../../middleware/userauth');
 const posts=require('../../models/Post')
 //get api/users
 //test route
+
+//route to get all the posts
+router.get('/all',userauth,async(req,res)=>{
+    try{
+        let allposts=await posts.find().sort({['date']:1});
+        res.send(allposts);
+    }catch(err){
+        res.status(400).send({err:"server error"});
+    }
+})
+
+
+//route to add new post
 router.post('/new',userauth,async (req,res)=>{
      try{
         let newpost=new posts({
             owned_id:req.body.user_id,
-            message:req.body.message
+            message:req.body.message,
+            date:Date.now()
          });
          await newpost.save();
          return res.send({msg:'post saved'});
@@ -19,7 +33,7 @@ router.post('/new',userauth,async (req,res)=>{
 });
 
 
-
+//route to delete already existing posts
 router.post('/delete',userauth,async (req,res)=>{
     try{
         let post=await posts.findById(req.body.post_id)
@@ -34,4 +48,28 @@ router.post('/delete',userauth,async (req,res)=>{
     }
 
 });
+
+router.post('/addcomment',userauth,async(req,res)=>{
+      try{
+         let post=await posts.findById(req.body.post_id);
+         post.comment.unshift({user:req.body.user_id,comment:req.body.comment});
+         await post.save();
+         console.log(post.comment);
+         res.send({msg:"commented to this post"})
+      }catch(err){
+        res.status(400).send({err:"server error"})
+      }
+})
+
+router.delete('/deletecomment',userauth,async(req,res)=>{
+    try{
+       let post=await posts.findById(req.body.post_id);
+       post.comment.filter((data)=>{data.user.toString()!== req.body.user_id;
+       })
+       await post.save();
+       res.send({msg:"comment on this post is deleted"})
+    }catch(err){
+      res.status(400).send({err:"server error"})
+    }
+})
 module.exports=router;
